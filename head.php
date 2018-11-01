0b9bb64c07e97036b947876fa760e5fadcd0ad65
js <>
2018-11-01 22:27:59 +0900

    오류 수정

--- head.php
+++ head.php
@@ -6,4 +6,4 @@
   <link rel="stylesheet" type="text/css" href="./css/style.css?ver=1.1" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
-</body>
+<body>
