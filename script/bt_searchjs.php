b860202ac0fe65dbe10087c1922a88051c68071f
js <>
2018-11-01 21:04:20 +0900

    검색버튼 hover, click 색 바뀜 실시간인기검색어 애니메이션 수정

---
+++ script/bt_searchjs.php
@@ -0,0 +1,13 @@
+<script>
+  $("#search-btn").hover(function(){
+    $("#search-btn").css("backgroundColor", "#d0c1d0")
+  },
+  function(){
+    console.log("unhover");
+    $("#search-btn").css("backgroundColor", "#504f57")
+  });
+  
+  $("#search-btn").click(function(){
+    $("#search-btn").css("backgroundColor", "#d8a3d2")
+  });
+</script>
--- script/dropdownjs.php
+++ script/dropdownjs.php
@@ -1,23 +1,19 @@
 <script> 
   $("#down_bt").mouseover(function(){
-    $(".hot-list").toggle();
-    $("#down_bt").css("display",'none');
-    $("#up_bt").css("display",'block');
-    $('.hot').css("background","white");
-    $('.hot').css("border",'0.04em solid #d8a3d2');        
-  });
-  $('#up_bt').mouseleave(function(){
-    $('.hot-list').toggle();
-    $('#down_bt').css('display', 'block');
-    $('#up_bt').css('display','none');
-    $('.hot').css("background","linear-gradient( to bottom, #F1EDF0, #FAF6F9 )");
-    $('.hot').css("border",'none');        
-  });
-  $(".hot").mouseover(function(){
-    $(".hot-list").toggle();
+    $(".hot-list").slideDown('fast');
     $("#down_bt").css("display",'none');
     $("#up_bt").css("display",'block');
     $('.hot').css("background","white");
     $('.hot').css("border",'0.04em solid #d8a3d2');
+    
+    $('.hot').hover(function(){
+      console.log("hover");
+    },function(){
+      $(".hot-list").slideUp('fast');
+      $('#down_bt').css('display', 'block');
+      $('#up_bt').css('display','none');
+      $('.hot').css("background","linear-gradient( to bottom, #F1EDF0, #FAF6F9 )");
+      $('.hot').css("border",'none');
+    });
   });
 </script>
