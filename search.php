47cfc1ad7f6d89b83c8ee9cf86f656b78afb010d
js <>
2018-11-03 17:02:38 +0900

    메인페이지 수정, 검색결과 페이지 생성

---
+++ search.php
@@ -0,0 +1,63 @@
+<?php $page = "search"; ?>
+<?php include_once('./head.php'); ?>
+<?php 
+if($_POST["search"]){
+  $search = $_POST["search"];
+}else{
+  $search = "";
+}
+$name="핫플레이스 이름입니다"
+
+?>
+<?php include_once("./head.php") ?>
+    <div class="main">
+      <div class="search-center">
+        <div class="main-top">
+          <div style="font-size: 2em;">
+            <div>
+              가장 인기있는
+            </div>
+            <div style="font-weight:bold">
+              핫플레이스
+            </div>
+          </div> 
+          <div class="search-time">
+            <a href ="search.php" method="POST" value="<?php echo $search; ?>">누적</a>
+            <a href ="">최근 1개월</a>
+          </div>
+        </div>
+        <?php 
+            $overname=$name;
+          if(strlen($name)>9){ 
+            $overname = str_replace($name,mb_substr($name,0,9,"utf-8")."...",$name);
+          }else{
+            $overname = $name;
+          }
+        ?>
+        <div class="search-list">
+          <div class="list-line">
+            <?php for($i=0; $i < 12; $i++){ ?>
+            <div class="store-list">
+              <div class="img-container">
+                <img src="./images/food1.jpeg" />
+              </div>
+              <div class="flex" style="padding:8px 4px;">
+                <div class="store-name"><?php echo $overname; ?></div>
+                <div class="views">2.4k</div>
+              </div>
+              <div class="flex" style="padding-left: 4px;">
+                <div class="keyword">
+                  <div class="chile-flex-1;">강남구</div>
+                </div>
+                <div class="keyword">
+                  <div class="chile-flex-1;">카페</div>
+                </div>
+              </div>
+            </div>
+            <?php } ?>
+          </div>
+        </div>
+      </div>  
+    </div>
+
+<?php include_once('./footer.php'); ?>
