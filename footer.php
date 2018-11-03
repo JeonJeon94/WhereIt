d05b8e15e7464a0bacaeeed4e1bb1fe8ac7ee5d3
js <>
2018-11-03 16:57:57 +0900

    haed,footer 파일 정리

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
+    <div class="footer">
+      <div class="info">
+        <div class="copyright">
+          Copyright 2018 BILLFISH Co,..,Ltd.All right reserved.
+        </div>
+        <div class="etc">
+          <div class="area-ok">검색가능지역</div>
+          <a href="./service-info.php">서비스정보</a>
+          <a href="./ask.php">문의하기</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
   <?php include_once("./script/slidejs.php") ?>
   <?php include_once("./script/dropdownjs.php") ?>
   <?php include_once("./script/bt_opacityjs.php") ?>
   <?php include_once("./script/bt_searchjs.php") ?> 
-  <?php include_once("./script/input_activejs.php") ?> 
+  <?php include_once("./script/input_activejs.php") ?>
 </html>
