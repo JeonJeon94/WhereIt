93e35a37c38a833e9e6c27e5c4ab5e4899763d25
js <>
2018-11-14 23:31:31 +0900

    검색페이지 디비로 연동

--- search.php
+++ search.php
@@ -1,10 +1,4 @@
 <?php $page = "search"; ?>
-<?php include_once('./head.php'); ?>
-<?php 
-
-$name="핫플레이스 이름입니다"
-
-?>
 <?php include_once("./head.php") ?>
     <div class="move-top">
       <img id="top" src="./images/top.png" />
@@ -23,41 +17,51 @@ $name="핫플레이스 이름입니다"
           </div> 
           <div class="search-time">
             <a id="all" style="color:black; font-weight:bold; border-bottom:2px solid black; padding-bottom:2px;" href ="">누적</a>
-            <a id="recently" href ="search_recently.php">최근 1개월</a>
+            <a id="recently" href ="search_recently.php?search=<?php echo $search; ?>">최근 1개월</a>
           </div>
         </div>
-        <?php 
-            $overname=$name;
-          if(strlen($name)>9){ 
-            $overname = str_replace($name,mb_substr($name,0,9,"utf-8")."...",$name);
-          }else{
-            $overname = $name;
-          }
-        ?>
         <div class="search-list">
           <div class="list-line">
-            <?php for($i=0; $i < 12; $i++){ ?>
-            <div class="store-list">
-              <div class="img-container" onclick="location.href='./detail.php'">
-                <img src="./images/food1.jpeg" />
-              </div>
-              <div class="flex" style="padding:8px 4px; width:232px;">
-                <div class="store-name"><?php echo $overname; ?></div>
-                <div class="views">2.4k</div>
-              </div>
-              <div class="flex" style="padding-left: 4px;">
-                <div class="keyword">
-                  <div class="chile-flex-1;">강남구</div>
-                </div>
-                <div class="keyword">
-                  <div class="chile-flex-1;">카페</div>
-                </div>
-              </div>
-            </div>
-            <?php } ?>
           </div>
         </div>
       </div>  
     </div>
+<script>
+  $(function(){
+    api_mongo_test("<?php echo $search; ?>",function(data){
+      var slot_template = _.template($("#store-slot").html());
+
+      for(var i =0; i < data.payload.length; i++){
+        var row = data.payload[i]
+        let nameDump = row.Name
+        nameDump = nameDump.length>8 ? nameDump.slice(0,8)+"..." : nameDump
+        row.Name = nameDump
+        try{
+          $(".list-line").append( slot_template(row) )
+        }catch(err){}  
+      }
+    })
+  })
+</script>
+
+<script id="store-slot" type="text/template">
+  <div class="store-list">
+    <div class="img-container" onclick="location.href='./detail.php?name=<%=Name%>'">
+      <img alt="food-img" src="<%=imgs[0].store_info[0].link%>" onerror="this.src='./images/desktop_detail_default.png'"/>
+    </div>
+    <div class="flex" style="padding:8px 4px; width:232px;">
+      <div class="store-name"><%=Name%></div>
+      <div class="views">2.4k</div>
+    </div>
+    <div class="flex" style="padding-left: 4px;">
+      <div class="keyword">
+        <div class="chile-flex-1;"><%=collect_region%></div>
+      </div>
+      <div class="keyword">
+        <div class="chile-flex-1;"><%=category[0]%></div>
+      </div>
+    </div>
+  </div>
+</script>
 
 <?php include_once('./footer.php'); ?>
