59372ca6a8651c52f15509fa2bd98814052e18bd
js <>
2018-11-15 20:49:29 +0900

    매거진 더 보기 버튼 클릭시 오류 수정

--- script/news_more_js.php
+++ script/news_more_js.php
@@ -2,5 +2,7 @@
   $('.more').click(function(){
     $('.news-detail').css('display', 'block')
     $('.news-list').css('display', 'none')
+    $('html').animate({scrollTop : 0}, 300);
+    return false;
   });
 </script>
