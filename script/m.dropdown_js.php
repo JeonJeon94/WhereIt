562279d885f244b24b4d58058ad6d7b761e183b0
js <>
2018-11-28 19:00:51 +0900

    모바일 실시간검색어 드롭다운 수정

--- script/m.dropdown_js.php
+++ script/m.dropdown_js.php
@@ -3,6 +3,7 @@
     $("#down-btn").css("display", "none")
     $("#up-btn").css("display", "block")
     $(".hot-list").slideDown('fast');
+    $('.hot-text').css('display', 'block')
     $(".hot").css("border", "0.04em solid #E0EAE9")
     $(".hot").css("background", "white")
   });
@@ -10,11 +11,11 @@
     $("#down-btn").css("display", "block")
     $("#up-btn").css("display", "none")
     $(".hot-list").slideUp("fast")
+    $('.hot-text').css('display', 'none')
     $(".hot").css("border", "none")
     $(".hot").css("background", "#f9f5f8")
   });
   $("#in-down_bt").on("click", function(){
-    console.log("down")
     $("#in-down_bt").css("display", "none")
     $("#in-up_bt").css("display", "block")
     $(".insta-text").slideDown('fast');
@@ -22,7 +23,6 @@
     $(".insta-list").css("background", "white")
   });
   $("#in-up_bt").on("click", function(){
-    console.log("up")
     $("#in-down_bt").css("display", "block")
     $("#in-up_bt").css("display", "none")
     $(".insta-text").slideUp("fast")
