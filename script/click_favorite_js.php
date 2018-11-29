403ae40cabe59539fb935274945e35039c1f34ba
js <>
2018-11-29 22:12:28 +0900

    pc 화면 클릭 기능 로그인, 회원가입에 맞춰 수정

--- script/click_favorite_js.php
+++ script/click_favorite_js.php
@@ -1,13 +1,22 @@
 <script>
-  
   jQuery(document).ready(function($){
     var b = false;
-    $(document).on("click", ".usage", function(){
-      b = !b
-      if( b == true){
-        $('.usage img').attr('src', './images/star_active.png')
+    var j = false;
+    // $(document).on("click", ".usage", function(){
+    //   // b = !b
+    //   // if( b == true){
+    //   //   $('.usage img').attr('src', './images/star_active.png')
+    //   // }else{
+    //   //   $('.usage img').attr('src', './images/star.png')
+    //   // }
+      
+    // });
+    $(document).on("click", ".share", function(){
+      j = !j
+      if( j == true){
+        $('.share img').attr('src', './images/ETC/share-active.png')
       }else{
-        $('.usage img').attr('src', './images/star.png')
+        $('.share img').attr('src', './images/share.png')
       }
     });
   });
--- script/send_click_js.php
+++ script/send_click_js.php
@@ -20,8 +20,23 @@
     $('.modal-container').css('display','none')
   });
   $('#next-btn2').click(function(){
-    $('.page-num2').css('display','none')
-    $('.page-num3').css('display','block')
+    if($('.checkbox3 img').attr('src') != "./images/box-check.png"){
+      return alert("약관에 동의하셔야 가입하실 수 있습니다.")
+    }
+    
+    $("#next-btn2").css("display","none")
+    api_send_mail($("#sign-mail").val(),$("#sign-pw").val(), $("#username").val(), function(res){
+      console.log(res)
+      // if(true){
+      if(res.code == 1){
+        $('.page-num2').css('display','none')
+        $('.signup-end').css('display','block')
+      }else{
+        alert("가입 도중 문제가 발생했습니다. 다시 시도해주세요!")
+        $("#next-btn2").css("display","block")
+      }
+    })
+    
   });
   $('#send-btn').click(function(){
     $('.page-num3').css('display','none')
@@ -36,16 +51,16 @@
     $('.myinfo').css('display','none')
     $('.edit-info').css('display','block')
   });
-  $('#save').click(function(){
-    $('.myinfo').css('display','block')
-    $('.edit-info').css('display','none')
-  });
+  // $('#save').click(function(){
+  //   $('.myinfo').css('display','block')
+  //   $('.edit-info').css('display','none')
+  // });
   $('#cancel').click(function(){
     $('.myinfo').css('display','block')
     $('.edit-info').css('display','none')
   });
-  $('#logout').click(function(){
-    $('.logout-box').css('display','block')
-    $('.modal-container').css('display','block')
-  });
+  // $('#logout').click(function(){
+  //   $('.logout-box').css('display','block')
+  //   $('.modal-container').css('display','block')
+  // });
 </script>
