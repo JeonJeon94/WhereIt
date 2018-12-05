76e1b3bad2c91bef57f7995024b098896ae7a215
js <>
2018-12-06 07:27:15 +0900

    비밀번호 수정 기능 모바일 버전 수정

--- edit_pw.php
+++ edit_pw.php
@@ -1,30 +1,30 @@
 <?php 
-include 'dbconfig.php';
+  include 'dbconfig.php';
 
-$arr_browser = array ("iPhone","iPod","IEMobile","Mobile","lgtelecom","PPC");
+  $arr_browser = array ("iPhone","iPod","IEMobile","Mobile","lgtelecom","PPC");
 
-for($indexi = 0 ; $indexi < count($arr_browser) ; $indexi++) {
- if(strpos($_SERVER['HTTP_USER_AGENT'],$arr_browser[$indexi]) == true){
-  // 모바일 브라우저라면  모바일 URL로 이동 
-   header("Location: ./m.edit_pwform.php?id=<?php echo $pw_search[id]?>");
-   exit;
- }
-}
+  $edit_pw=$_GET['code'];
 
-$edit_pw=$_GET['code'];
+  $pw_search = sql_one("SELECT id FROM users WHERE reset_password = '$edit_pw'");
 
-$pw_search = sql_one("SELECT id FROM users WHERE reset_password = '$edit_pw'");
+  if($pw_search){
+    $true = '';
+    sql_query("UPDATE users SET
+    reset_password='$true',
+    password=''
+    WHERE id='$pw_search[id]'");
 
-if($pw_search){
-  $true = '';
-  sql_query("UPDATE users SET
-  reset_password='$true',
-  password=''
-  WHERE id='$pw_search[id]'");
-?>
-  <script>location.href='./edit_pwform.php?id=<?php echo $pw_search[id] ?>'</script>
-<?php
-}else{
-  alert_back("잘못된 접근입니다!");
-}
+    for($indexi = 0 ; $indexi < count($arr_browser) ; $indexi++) {
+      if(strpos($_SERVER['HTTP_USER_AGENT'],$arr_browser[$indexi]) != false){
+        // 모바일 브라우저라면  모바일 URL로 이동 
+        header("Location: ./m.edit_pwform.php?id=$pw_search[id]");
+        exit;
+      } else {
+        header("Location: ./edit_pwform.php?id=$pw_search[id]");
+      }
+    }
+  }else{
+    alert_back("잘못된 접근입니다!");
+  }
+
 ?>
