9e543d9269e8f40c2705724d5360415760cf14e6
js <>
2018-12-06 10:37:34 +0900

    모바일 회원가입 이메일 인증 오류 수정

--- email_verification.php
+++ email_verification.php
@@ -1,27 +1,26 @@
 <?php 
-include 'dbconfig.php';
+  include 'dbconfig.php';
 
-$arr_browser = array ("iPhone","iPod","IEMobile","Mobile","lgtelecom","PPC");
+  $arr_browser = array ("iPhone","iPod","IEMobile","Mobile","lgtelecom","PPC");
 
-for($indexi = 0 ; $indexi < count($arr_browser) ; $indexi++) {
- if(strpos($_SERVER['HTTP_USER_AGENT'],$arr_browser[$indexi]) == true){
-  // 모바일 브라우저라면  모바일 URL로 이동 
-   header("Location: ./m.index.php");
-   exit;
- }
-}
+  $login_email=$_GET['code'];
 
-$login_email=$_GET['code'];
+  $email_search = sql_one("SELECT id FROM users WHERE email_code = '$login_email'");
 
-$email_search = sql_one("SELECT id FROM users WHERE email_code = '$login_email'");
-
-if($email_search){
-  $true = '';
-  sql_query("UPDATE users SET email_code='$true' WHERE id='$email_search[id]'");
-?>
-  <script>location.href='/index.php'</script>
-<?php
-}else{
-  alert_back("잘못된 접근입니다!");
-}
+  if($email_search){
+    $true = '';
+    sql_query("UPDATE users SET email_code='$true' WHERE id='$email_search[id]'");
+    
+    for($indexi = 0 ; $indexi < count($arr_browser) ; $indexi++) {
+      if(strpos($_SERVER['HTTP_USER_AGENT'],$arr_browser[$indexi]) == true){
+      // 모바일 브라우저라면  모바일 URL로 이동 
+        header("Location: ./m.index.php");
+        exit;
+      }else{
+      header("Location: ./index.php");
+      }
+    }
+  }else{
+    alert_back("잘못된 접근입니다!");
+  }
 ?>
