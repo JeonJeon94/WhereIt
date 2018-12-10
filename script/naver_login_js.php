2cf1e22571882e08e7039160668f7ab26fd099a1
js <>
2018-12-10 15:07:43 +0900

    테스트2

--- script/naver_login_js.php
+++ script/naver_login_js.php
@@ -2,7 +2,7 @@
 	var naverLogin = new naver.LoginWithNaverId(
 		{
 			clientId: "T2gAKVtQHVUdwkqP3sew",
-			callbackUrl: "http://http://13.209.76.121/naver.login.php",
+			callbackUrl: "http://13.209.76.121/naver.login.php",
       isPopup: false, /* 팝업을 통한 연동처리 여부 */
 			loginButton: {color: "white", type: 1, height: 34} /* 로그인 버튼의 타입을 지정 */
 		}
