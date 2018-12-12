e2ea8e3b84f8b4493e290bcf325c52fe061dea66
js <>
2018-12-13 02:16:21 +0900

    a

--- script/naver_login_js.php
+++ script/naver_login_js.php
@@ -2,7 +2,7 @@
 	var naverLogin = new naver.LoginWithNaverId(
 		{
 			clientId: "T2gAKVtQHVUdwkqP3sew",
-			callbackUrl: "http://whereit.kr//naver.login.php",
+			callbackUrl: "http://www.whereit.kr//naver.login.php",
       isPopup: false, /* 팝업을 통한 연동처리 여부 */
 			loginButton: {color: "white", type: 1, height: 34} /* 로그인 버튼의 타입을 지정 */
 		}
