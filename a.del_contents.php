9e9cee92404427e693820fd94147683e4d141fe5
js <>
2019-01-02 19:56:58 +0900

    a

--- a.del_contents.php
+++ a.del_contents.php
@@ -3,8 +3,14 @@
 
   if(!empty($_GET['id'])){
     $Ids=$_GET['id'];
-
+    
+    $sql = sql_one("SELECT * FROM contents WHERE id in ($Ids)");
     $del_id = sql_query("DELETE FROM contents WHERE id in ($Ids)");
+
+    $sub = explode(',', $sql[sub]);
+
+    unlink($sql[main]);
+    for($i=1; $i<count($sub); $i++){ unlink($sub[$i]); }
     php_redirect('./a.contents.php');
   }else{
     alert_back('삭제할 컨텐츠를 선택해주세요.');
