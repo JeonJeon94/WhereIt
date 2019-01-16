7875c431bc146bd971145a5124655f43f3d3a5ea
js <>
2019-01-16 22:38:21 +0900

    a

--- m.service_info.php
+++ m.service_info.php
@@ -2,7 +2,7 @@
 <?php include_once('./m.head.php') ?>
     <div class="main">
       <div class="service-info">
-        <img src="images/whererit_service_img_2.png" />
+        <img src="images/whereit_service_img_2.png" />
       </div>
     </div>
 <?php include_once('./m.footer.php') ?>
