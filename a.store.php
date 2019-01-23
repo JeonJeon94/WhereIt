a75e72bf57f7f16ccdf665d02d9346c92b9856a3
js <>
2019-01-23 21:24:10 +0900

    a

--- a.store.php
+++ a.store.php
@@ -127,7 +127,7 @@
     var rank_templete = _.template($("#store-slot").html());
 
     function load(i){
-      api_shop_list(i, 30, function(res){
+      api_shop_list(i, 4000, function(res){
         data = res
         if(data.payload !== undefined){
           for(let j=0;j<data.payload.length;j++){
